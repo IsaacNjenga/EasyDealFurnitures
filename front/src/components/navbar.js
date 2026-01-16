b78bcd7c58f0080fb40a34eae043a7d3ba0f0394
Isaac Njenga <ayzzoh20@gmail.com>
2026-01-16 16:18:50 +0300

    nakupenda Mungu

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -520,7 +520,7 @@ function Navbar() {
           borderRadius: "50%",
           boxShadow: "0 8px 24px rgba(0, 0, 0, 0.15)",
           zIndex: 1000,
-          display: openChat ? "none" : "flex",
+          display: !streamLoading && openChat ? "none" : "flex",
           flexDirection: "column",
           overflow: "hidden",
         }}
